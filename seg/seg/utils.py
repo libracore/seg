f25a4ed5d87be253bf184c4a66ad974e069f4dba
lasalesi <larsericmueller@gmail.com>
2021-11-01 23:47:52 +0100

    variant matching

--- seg/seg/utils.py
+++ seg/seg/utils.py
@@ -5,27 +5,44 @@
 import frappe
 import json
 import six
+from erpnext.portal.product_configurator.utils import get_next_attribute_and_values
 
 @frappe.whitelist()
 def get_total_weight(items, qtys, kgperL=1.5):
-	total_weight = 0
-	if isinstance(items, six.string_types):
-		items = json.loads(items)
-	if isinstance(qtys, six.string_types):
-		qtys = json.loads(qtys)
-	i = 0
-	while i < len(items):
-		doc = frappe.get_doc("Item", items[i])
-		if doc != None:
-			if doc.weight_per_unit > 0 and doc.has_weight:
-				if doc.weight_uom == "kg":
-					total_weight += qtys[i] * doc.weight_per_unit
-				elif doc.weight_uom == "L":
-					# make sure, tabItem contains custom field (float) density!
-					total_weight += qtys[i] * doc.density * doc.weight_per_unit
-		i += 1
-	return { 'total_weight': total_weight }
+    total_weight = 0
+    if isinstance(items, six.string_types):
+        items = json.loads(items)
+    if isinstance(qtys, six.string_types):
+        qtys = json.loads(qtys)
+    i = 0
+    while i < len(items):
+        doc = frappe.get_doc("Item", items[i])
+        if doc != None:
+            if doc.weight_per_unit > 0 and doc.has_weight:
+                if doc.weight_uom == "kg":
+                    total_weight += qtys[i] * doc.weight_per_unit
+                elif doc.weight_uom == "L":
+                    # make sure, tabItem contains custom field (float) density!
+                    total_weight += qtys[i] * doc.density * doc.weight_per_unit
+        i += 1
+    return { 'total_weight': total_weight }
 
 @frappe.whitelist()
 def get_user_image(user):
     return frappe.get_value("User", user, "user_image")
+
+@frappe.whitelist()
+def get_matching_variant(item_code, old_selection, new_selection):
+    attributes = new_selection
+    for a in old_selection:
+        # check if there are multiple variants matching
+        matches = get_next_attribute_and_values(item_code, attributes)
+        if len(matches.filtered_items) == 1:
+            # leave with this variant code
+            return matches.filtered_items[0]
+        # refine search, add attribute
+        for k,v in a.items():
+            if k not in attributes:
+                attributes[k] = v
+    # nothing found
+    return None
