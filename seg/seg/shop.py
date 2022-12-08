485a5eb78cf3a9c3e954c7404140240108a32912
lasalesi <larsericmueller@gmail.com>
2022-12-08 14:53:29 +0100

    capture errors on get_prices

--- seg/seg/shop.py
+++ seg/seg/shop.py
@@ -37,60 +37,63 @@ def get_matching_variant(item_code, old_selection, new_selection):
 @frappe.whitelist()
 def get_prices(item_code, user):
     from erpnext.controllers.website_list_for_contact import get_customers_suppliers
-    if user:
-        customers, suppliers = get_customers_suppliers("Sales Invoice", user)
-        if len(customers) > 0:
-            customer = customers[0]
+    try:
+        if user:
+            customers, suppliers = get_customers_suppliers("Sales Invoice", user)
+            if len(customers) > 0:
+                customer = customers[0]
+            else:
+                customer  = "None"
         else:
-            customer  = "None"
-    else:
-        customer = "None"
-    # find recursive item groups
-    item_groups = get_recursive_item_groups(frappe.get_value("Item", item_code, "item_group"))
-    sql_query = """SELECT 
-            `raw`.`item_code`,
-            `raw`.`item_name`,
-            `raw`.`item_group`,
-            GROUP_CONCAT(`tabItem Variant Attribute`.`attribute_value`) AS `attributes`,
-            `raw`.`stock_uom`,
-            ROUND(`raw`.`price_list_rate`, 2) AS `price_list_rate`,
-            `raw`.`pricing_rule`,
-            `tPR`.`discount_percentage` AS `discount_percentage`,
-            ROUND(((100 - IFNULL(`tPR`.`discount_percentage`, 0))/100) * `raw`.`price_list_rate`, 2) AS `discounted_rate`
-        FROM 
-            (SELECT 
-              `tabItem`.`item_code` AS `item_code`,
-              `tabItem`.`item_name` AS `item_name`,
-              `tabItem`.`item_group` AS `item_group`,
-              `tabItem`.`last_purchase_rate` AS `last_purchase_rate`,
-              CONCAT(ROUND(`tabItem`.`weight_per_unit`, 1), " ", `tabItem`.`weight_uom`) AS `stock_uom`,
-              (SELECT `tabItem Price`.`price_list_rate` 
-               FROM `tabItem Price` 
-               WHERE `tabItem Price`.`item_code` = `tabItem`.`item_code`
-                 AND `tabItem Price`.`price_list` = "Standard-Vertrieb") AS `price_list_rate`,
-              (SELECT `tabPricing Rule`.`name`
-               FROM `tabPricing Rule`
-               LEFT JOIN `tabPricing Rule Item Code` ON `tabPricing Rule Item Code`.`parent` = `tabPricing Rule`.`name`
-               LEFT JOIN `tabPricing Rule Item Group` ON `tabPricing Rule Item Group`.`parent` = `tabPricing Rule`.`name`
-               WHERE `tabPricing Rule`.`selling` = 1
-                 AND `tabPricing Rule`.`customer` = "{customer}"
-                 AND `tabPricing Rule`.`disable` = 0
-                 AND (`tabPricing Rule Item Code`.`item_code` = `tabItem`.`item_code`
-                      OR `tabPricing Rule Item Group`.`item_group` IN ({item_groups})
-                      OR `tabPricing Rule Item Group`.`item_group` = "Alle Artikelgruppen")
-               ORDER BY `tabPricing Rule`.`priority` DESC
-               LIMIT 1) AS `pricing_rule`
-            FROM `tabItem`
-            WHERE `tabItem`.`item_code` = "{item_code}"
-              OR `tabItem`.`variant_of` = "{item_code}"
-            ) AS `raw`
-        LEFT JOIN `tabPricing Rule` AS `tPR` ON `tPR`.`name` = `raw`.`pricing_rule`
-        LEFT JOIN `tabItem Variant Attribute` ON `raw`.`item_code` = `tabItem Variant Attribute`.`parent`
-        GROUP BY `raw`.`item_code`
-    """.format(customer=customer, item_code=item_code, item_groups=", ".join('"{w}"'.format(w=w) for w in item_groups))
-    data = frappe.db.sql(sql_query, as_dict=True)
-    return data
-
+            customer = "None"
+        # find recursive item groups
+        item_groups = get_recursive_item_groups(frappe.get_value("Item", item_code, "item_group"))
+        sql_query = """SELECT 
+                `raw`.`item_code`,
+                `raw`.`item_name`,
+                `raw`.`item_group`,
+                GROUP_CONCAT(`tabItem Variant Attribute`.`attribute_value`) AS `attributes`,
+                `raw`.`stock_uom`,
+                ROUND(`raw`.`price_list_rate`, 2) AS `price_list_rate`,
+                `raw`.`pricing_rule`,
+                `tPR`.`discount_percentage` AS `discount_percentage`,
+                ROUND(((100 - IFNULL(`tPR`.`discount_percentage`, 0))/100) * `raw`.`price_list_rate`, 2) AS `discounted_rate`
+            FROM 
+                (SELECT 
+                  `tabItem`.`item_code` AS `item_code`,
+                  `tabItem`.`item_name` AS `item_name`,
+                  `tabItem`.`item_group` AS `item_group`,
+                  `tabItem`.`last_purchase_rate` AS `last_purchase_rate`,
+                  CONCAT(ROUND(`tabItem`.`weight_per_unit`, 1), " ", `tabItem`.`weight_uom`) AS `stock_uom`,
+                  (SELECT `tabItem Price`.`price_list_rate` 
+                   FROM `tabItem Price` 
+                   WHERE `tabItem Price`.`item_code` = `tabItem`.`item_code`
+                     AND `tabItem Price`.`price_list` = "Standard-Vertrieb") AS `price_list_rate`,
+                  (SELECT `tabPricing Rule`.`name`
+                   FROM `tabPricing Rule`
+                   LEFT JOIN `tabPricing Rule Item Code` ON `tabPricing Rule Item Code`.`parent` = `tabPricing Rule`.`name`
+                   LEFT JOIN `tabPricing Rule Item Group` ON `tabPricing Rule Item Group`.`parent` = `tabPricing Rule`.`name`
+                   WHERE `tabPricing Rule`.`selling` = 1
+                     AND `tabPricing Rule`.`customer` = "{customer}"
+                     AND `tabPricing Rule`.`disable` = 0
+                     AND (`tabPricing Rule Item Code`.`item_code` = `tabItem`.`item_code`
+                          OR `tabPricing Rule Item Group`.`item_group` IN ({item_groups})
+                          OR `tabPricing Rule Item Group`.`item_group` = "Alle Artikelgruppen")
+                   ORDER BY `tabPricing Rule`.`priority` DESC
+                   LIMIT 1) AS `pricing_rule`
+                FROM `tabItem`
+                WHERE `tabItem`.`item_code` = "{item_code}"
+                  OR `tabItem`.`variant_of` = "{item_code}"
+                ) AS `raw`
+            LEFT JOIN `tabPricing Rule` AS `tPR` ON `tPR`.`name` = `raw`.`pricing_rule`
+            LEFT JOIN `tabItem Variant Attribute` ON `raw`.`item_code` = `tabItem Variant Attribute`.`parent`
+            GROUP BY `raw`.`item_code`
+        """.format(customer=customer, item_code=item_code, item_groups=", ".join('"{w}"'.format(w=w) for w in item_groups))
+        data = frappe.db.sql(sql_query, as_dict=True)
+        return data
+    except Exception as err:
+        return {'error': err}
+        
 @frappe.whitelist(allow_guest=True)
 def get_public_prices(item_code):
     return get_prices(item_code, None)
