1153bd7b489aabd5ffe94e66b5dcf2b73f5402db
lasalesi <larsericmueller@gmail.com>
2023-06-23 12:09:39 +0200

    hotfix menu

--- seg/public/js/seg_common.js
+++ seg/public/js/seg_common.js
@@ -5,21 +5,20 @@
 // 1 sec after start (has to be delayed after document ready)
 window.onload = async function () {
     setTimeout(function() {
-            if (window.location.hostname.includes("/desk#modules/SEG")) {
-                create_customized_menu();
-                console.log("menu created");
-            }
+        if (window.location.toString().includes("/desk#modules/SEG")) {
+            create_customized_menu();
+            console.log("menu created");
         }
     }, 1000);
 }
 
 function create_customized_menu() {
-    create_menu("create_sample_nic", "Musterbezug Nic", "6710 - Materialentnahme Nic - SEG")
-    create_menu("create_sample_chris", "Musterbezug Christian", "6711 - Materialentnahme Christian - SEG")
-    create_menu("create_sample_ben", "Musterbezug Beni", "6712 - Materialentnahme Beni - SEG")
+    create_menu("create_sample_nic", "Musterbezug Nic", "6710 - Materialentnahme Nic - SEG");
+    create_menu("create_sample_chris", "Musterbezug Christian", "6711 - Materialentnahme Christian - SEG");
+    create_menu("create_sample_ben", "Musterbezug Beni", "6712 - Materialentnahme Beni - SEG");
 }
 
-function create_menu(endpoint, title account) {
+function create_menu(endpoint, title, account) {
     var menu_entry_parent = $("a:contains('" + endpoint + "')").parent();
     menu_entry_parent.removeClass("flush-top");
     menu_entry_parent.html("");
