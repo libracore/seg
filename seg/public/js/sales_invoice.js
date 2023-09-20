44186bf3f61509f47987679c1bc1c61eaf670ca7
Valentina <vjss05@gmail.com>
2023-09-20 09:21:26 +0200

    Update des Mahnsperrenskripts

--- seg/public/js/sales_invoice.js
+++ seg/public/js/sales_invoice.js
@@ -1,13 +1,28 @@
 frappe.ui.form.on('Sales Invoice',  {
     before_save: function(frm) {
-		setTimeout(function () {  
-			if (frm.doc.mahnsperre === 1) {
+		check_customer_mahnsperre(frm)
+    }
+});
+
+function check_customer_mahnsperre(frm) {
+	frappe.call({
+		"method": "frappe.client.get_list",
+		"args": {
+			"doctype": "Customer",
+			'filters': [
+         	    ["name","IN", [frm.doc.customer]]
+         	],
+			"fields": ["mahnsperre"]
+		},
+		"callback": function(response) {
+			var mahnsperre = response.message[0].mahnsperre;
+
+			if (mahnsperre === 1) {
 				cur_frm.set_value("exclude_from_payment_reminder_until", "2099-12-31");
 			} else if (frm.doc.is_return === 0) {
 				// do not remind 20 days
 				cur_frm.set_value("exclude_from_payment_reminder_until",frappe.datetime.add_days(frm.doc.due_date, 20));
 			}
-        }, 800);
-                            
-    }
-});
+		}
+	});
+}
