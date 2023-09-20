4bd1e2cb830325a611e52ffb7fbeb83d914e43a0
Valentina <vjss05@gmail.com>
2023-09-20 09:53:12 +0200

    Mahnblockabruf bei Refresh

--- seg/public/js/sales_invoice.js
+++ seg/public/js/sales_invoice.js
@@ -1,6 +1,14 @@
 frappe.ui.form.on('Sales Invoice',  {
-    before_save: function(frm) {
+	refresh: function(frm) {
 		check_customer_mahnsperre(frm)
+	},
+    before_save: function(frm) {
+		if (frm.doc.mahnsperre === 1) {
+			cur_frm.set_value("exclude_from_payment_reminder_until", "2099-12-31");
+		} else if (frm.doc.is_return === 0) {
+			// do not remind 20 days
+			cur_frm.set_value("exclude_from_payment_reminder_until",frappe.datetime.add_days(frm.doc.due_date, 20));
+		}
     }
 });
 
@@ -16,12 +24,8 @@ function check_customer_mahnsperre(frm) {
 		},
 		"callback": function(response) {
 			var mahnsperre = response.message[0].mahnsperre;
-
 			if (mahnsperre === 1) {
-				cur_frm.set_value("exclude_from_payment_reminder_until", "2099-12-31");
-			} else if (frm.doc.is_return === 0) {
-				// do not remind 20 days
-				cur_frm.set_value("exclude_from_payment_reminder_until",frappe.datetime.add_days(frm.doc.due_date, 20));
+				cur_frm.set_value("mahnsperre", 1);
 			}
 		}
 	});
