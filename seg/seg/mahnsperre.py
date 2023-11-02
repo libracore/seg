71dd0d6d902d7ef95631210a793af8724c1f0f4a
Valentina <vjss05@gmail.com>
2023-11-02 09:31:46 +0100

    fixing filter for set_mahnsperre

--- seg/seg/mahnsperre.py
+++ seg/seg/mahnsperre.py
@@ -6,18 +6,16 @@ from frappe.utils import cint
 
 @frappe.whitelist()
 def set_mahnsperre(mahnsperre, customer):
-    invoices = frappe.get_all("Sales Invoice", filters={"customer": customer, "docstatus": ["!=", 2], "status": ["!=", "Paid"]})
+    invoices = frappe.get_all("Sales Invoice", filters={"customer": customer, "docstatus": ["!=", 2], "status": ["not in", ["Paid", "Return", "Credit Note Issued"]]})
     for invoice in invoices:
         doc = frappe.get_doc("Sales Invoice", invoice.name)
-        if doc.status != "Credit Note Issued" and doc.status != "Return":
-            if cint(mahnsperre) == 1:
-                doc.mahnsperre = 1
-                doc.exclude_from_payment_reminder_until = "2099-12-31"
-                doc.save()
-            else:
-                if doc.due_date:
-                    doc.mahnsperre = 0
-                    # Calculate the new date based on "due_date" + 20 days
-                    new_date = frappe.utils.add_days(doc.due_date, 20)
-                    doc.exclude_from_payment_reminder_until = new_date.strftime("%Y-%m-%d")
-                    doc.save()
+        if cint(mahnsperre) == 1:
+            doc.mahnsperre = 1
+            doc.exclude_from_payment_reminder_until = "2099-12-31"
+        else:
+            if doc.due_date:
+                doc.mahnsperre = 0
+                # Calculate the new date based on "due_date" + 20 days
+                new_date = frappe.utils.add_days(doc.due_date, 20)
+                doc.exclude_from_payment_reminder_until = new_date.strftime("%Y-%m-%d")
+        doc.save()
