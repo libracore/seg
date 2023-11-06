70bfe327417388bcbd5c2e7e7e00a6a53e3fc6cb
Valentina <vjss05@gmail.com>
2023-11-06 11:59:56 +0100

    Showing Button only when ignore_pricing_rule is not set

--- seg/public/js/delivery_note.js
+++ seg/public/js/delivery_note.js
@@ -3,61 +3,54 @@ frappe.ui.form.on('Delivery Note', {
         frm.add_custom_button(__("Umlagern"), function() {
             move_stock(frm);
         });
-        frm.add_custom_button(__("Detach From Pricing Rule"), function() {
-            if (frm.doc.ignore_pricing_rule) {
-				// In case the user has set first "Ignore Pricing Rule"
-				show_ignore_pricing_rule_message();
-			} else {
-				modify_item_rate(frm);
-			}
-        });
+        if (!frm.doc.ignore_pricing_rule) {
+            frm.add_custom_button(__("Detach From Pricing Rule"), function() {
+                modify_item_rate(frm);
+            });
+        }
     },
     before_save: function(frm) {
-		//calculate the wir_percent and wir_amount for each item
-		if (frm.doc.wir_percent > 0) {
-			update_wir_for_each_item(frm);
-		}
-	},
-	ignore_pricing_rule: function(frm) {
-		// When unchecking the "Ignore Pricing Rule" after "Deatch Prices" the rate with the rule remains so this will bring back the original selling rate
-		if (!frm.doc.ignore_pricing_rule) {
-			return_to_previous_rate(frm);
-		} 
-	}
+        //calculate the wir_percent and wir_amount for each item
+        if (frm.doc.wir_percent > 0) {
+            update_wir_for_each_item(frm);
+        }
+    },
+    ignore_pricing_rule: function(frm) {
+        // When unchecking the "Ignore Pricing Rule" after "Deatch Prices" the rate with the rule remains so this will bring back the original selling rate
+        if (!frm.doc.ignore_pricing_rule) {
+            return_to_previous_rate(frm);
+        } 
+    }
 })
 
-function show_ignore_pricing_rule_message(){
-	frappe.msgprint("Please unchecked 'Ignore Pricing Rule' and Save the document before enabling this.");
-}
-
 function modify_item_rate(frm) {
-	frm.doc.items.forEach(function (item) {
-		var price_rule_rate = item.rate;
-		frappe.model.set_value(item.doctype, item.name, 'discount_percentage', 0);
-		frappe.model.set_value(item.doctype, item.name, 'price_list_rate', price_rule_rate);
-	});
-	cur_frm.set_value("ignore_pricing_rule", 1);
+    frm.doc.items.forEach(function (item) {
+        var price_rule_rate = item.rate;
+        frappe.model.set_value(item.doctype, item.name, 'discount_percentage', 0);
+        frappe.model.set_value(item.doctype, item.name, 'price_list_rate', price_rule_rate);
+    });
+    cur_frm.set_value("ignore_pricing_rule", 1);
 }
 
 function return_to_previous_rate(frm){
-	frm.doc.items.forEach(function (item) {
-		frappe.call({
-			"method": "frappe.client.get",
-			"args": {
-				"doctype": "Item Price",
-				"filters": {
-					"item_code": item.item_code,
-					"selling": 1
-				}
-			},
-			"callback": function(response) {
-				if (response) {
-					var item_price_list_rate = response.message.price_list_rate;
-					frappe.model.set_value(item.doctype, item.name, 'price_list_rate', item_price_list_rate);
-				}
-			}
-		});
-	});
+    frm.doc.items.forEach(function (item) {
+        frappe.call({
+            "method": "frappe.client.get",
+            "args": {
+                "doctype": "Item Price",
+                "filters": {
+                    "item_code": item.item_code,
+                    "selling": 1
+                }
+            },
+            "callback": function(response) {
+                if (response) {
+                    var item_price_list_rate = response.message.price_list_rate;
+                    frappe.model.set_value(item.doctype, item.name, 'price_list_rate', item_price_list_rate);
+                }
+            }
+        });
+    });
 }
 
 function move_stock(frm) {
@@ -143,8 +136,8 @@ function move_stock(frm) {
 }
 
 function update_wir_for_each_item(frm) {
-	frm.doc.items.forEach(function(item) {
+    frm.doc.items.forEach(function(item) {
         frappe.model.set_value("Delivery Note Item", item.name, "wir_percent_on_item", (frm.doc.wir_percent / frm.doc.items.length));
-		frappe.model.set_value("Delivery Note Item", item.name, "wir_amount_on_item", (frm.doc.wir_amount * (item.net_amount / frm.doc.net_total)));
+        frappe.model.set_value("Delivery Note Item", item.name, "wir_amount_on_item", (frm.doc.wir_amount * (item.net_amount / frm.doc.net_total)));
    });
 }
