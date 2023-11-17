0624f8540783fcd1b4c24364e3ddfe1923016a10
lasalesi <larsericmueller@gmail.com>
2023-11-17 16:14:12 +0100

    credits to advances draft

--- seg/seg/utils.py
+++ seg/seg/utils.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017-2022, libracore AG and Contributors
+# Copyright (c) 2017-2023, libracore AG and Contributors
 # License: GNU General Public License v3. See license.txt
 
 # customisation for total weight calculation
@@ -7,6 +7,7 @@ import json
 import six
 from erpnext.portal.product_configurator.utils import get_next_attribute_and_values
 from seg.seg.doctype.sales_report.sales_report import update_last_purchase_rates
+from datetime import datetime
 
 @frappe.whitelist()
 def get_total_weight(items, qtys, kgperL=1.5):
@@ -53,3 +54,49 @@ def create_stock_entry(stock_entry_type, items, warehouse, qty, base_rate=None):
     if stock_entry_type == "Material Receipt":
         update_last_purchase_rates(doc.name)
     return doc
+
+def convert_credits_to_advances():
+    invoices_with_credits = frappe.get_all("Sales Invoice",
+        filters=[['outstanding_amount', '<', 0], ['docstatus', '=', 1]],
+        fields=['name']
+    )
+    
+    for invoice in invoices_with_credits:
+        transfer_credit(invoice.get("name"))
+        
+    return
+    
+def transfer_credit(sales_invoice_name):
+    sales_invoice = frappe.get_doc("Sales Invoice", sales_invoice_name)
+    outstanding_amount = (-1) * sales_invoice.outstanding_amount
+    if outstanding_amount >= 0:
+        return
+    
+    debit_account = sales_invoice.debit_to
+    jv = frappe.get_doc({
+        'doctype': "Journal Entry",
+        'posting_date': datetime.now(),
+        'accounts': [
+            {
+                'account': debit_account,
+                'party_type': "Customer",
+                'party': sales_invoice.customer,
+                'debit_in_account_currency': outstanding_amount,
+                'reference_type': "Sales Invoice",
+                'reference_name': sales_invoice_name
+            },
+            {
+                'account': debit_account,
+                'party_type': "Customer",
+                'party': sales_invoice.customer,
+                'credit_in_account_currency': outstanding_amount,
+                'is_advance': "Yes"
+            }
+        ],
+        'user_remarks': "Umbuchung von {0}".format(sales_invoice_name),
+        'company': sales_invoice.company
+    })
+    jv.insert()
+    jv.submit()
+    frappe.db.commit()
+    
