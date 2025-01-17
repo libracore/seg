5a22899dff08b646cf484e01b45326ec1acae2d7
lasalesi <larsericmueller@gmail.com>
2025-01-17 22:36:10 +0100

    adding settings and working folder creation

--- seg/seg/nextcloud.py
+++ seg/seg/nextcloud.py
@@ -4,7 +4,8 @@
 from __future__ import unicode_literals
 import frappe
 import os
-from nc_py_api import Nextcloud
+#from nc_py_api import Nextcloud        # requires Py3.9 or higher :-(
+from webdav3.client import Client
 from frappe.utils.password import get_decrypted_password
 from frappe.desk.form.load import get_attachments
 import time
@@ -37,13 +38,38 @@ This is the authentication function
 def get_client():
     settings = frappe.get_doc("SEG Settings", "SEG Settings")
     password = get_decrypted_password("SEG Settings", "SEG Settings", "cloud_password")
-    nc = Nextcloud(
-        nextcloud_url=settings.cloud_hostname, 
-        nc_auth_user=settings.cloud_user, 
-        nc_auth_pass=password
-    )
+    #nc = Nextcloud(
+    #    nextcloud_url=settings.cloud_hostname, 
+    #    nc_auth_user=settings.cloud_user, 
+    #    nc_auth_pass=password
+    #)
+    
+    options = {
+        'webdav_hostname': "{0}/remote.php".format(settings.cloud_hostname),
+        'webdav_root': "files/",
+        'webdav_login': settings.cloud_user,
+        'webdav_password': password
+    }
+    nc = Client(options)
+
     return nc
 
+"""
+Initialise
+"""
+def initialise():
+    if cint(frappe.get_value("SEG Settings", "SEG Settings", "nextcloud_enabled")) == 0:
+        return      # skip if nextcloud is disabled (develop environments)
+        
+    client = get_client()
+    
+    storage_folder = get_base_path()
+    client.mkdir(storage_folder)
+    for k,v in PATHS.items():
+        client.mkdir(os.path.join(storage_folder, k))
+        
+    return
+    
 """
 This function will create a new folder with the required structure
 """
@@ -56,7 +82,7 @@ def create_folder(doctype, name):
     storage_path = get_storage_path(doctype, name)
     
     create_path(client, storage_path)
-    for k, v in PATHS[doctype]:
+    for k, v in PATHS[doctype].items():
         # create child folders
         create_path(client, os.path.join(storage_path, PATHS[doctype][k]))
     
@@ -81,7 +107,9 @@ def create_path(client, path):
         
     # create folder
     try:
-        client.files.mkdir(path)
+        #client.files.mkdir(path)
+        if not client.check(path):
+            client.mkdir(path)
     except Exception as err:
         frappe.throw("{0}: {1}".format(path, err), "Create project folder (NextCloud")
     return
@@ -96,8 +124,18 @@ def upload_from_local_file(doctype, name, target, file_name):
         
     client = get_client()
     storage_path = get_storaget_path(doctype, name)
+    target_path = os.path.join(storage_path, target)
     
-    client.files.upload_stream(os.path.join(storage_path, target, file_name.split("/")[-1]), file_name)
+    #client.files.upload_stream(os.path.join(storage_path, target, file_name.split("/")[-1]), file_name)
+    if client.check(os.path.join(storage_path, target)):
+        client.upload_sync(os.path.join(target_path, file_name.split("/")[-1]), file_name)
+    else:
+        # try to create the requested folder
+        try:
+            create_path(client, target_path)
+            client.upload_sync(os.path.join(target_path, file_name.split("/")[-1]), file_name)
+        except:
+            frappe.throw("Unable to upload {0} to {1}".format(file_name, target_path))
 
     return
 
