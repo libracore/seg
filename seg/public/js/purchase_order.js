59a8ce76fe00821cafcef3f42b7ec722ecddfe5d
Ivanios <ivan.lochbihler@libracore.com>
2025-02-25 19:04:42 +0100

    updated purchasing process

--- seg/public/js/purchase_order.js
+++ seg/public/js/purchase_order.js
@@ -13,5 +13,38 @@ frappe.ui.form.on('Purchase Order',  {
                 $(target).parent().parent().remove();   // remove Menu > Email
             }
         }
+        
+       frm.set_query('drop_ship_reference', function() {
+            return {
+                filters: {
+                    'docstatus': 1
+                }
+            };
+        });
+    },
+    drop_ship_reference: function(frm) {
+        set_drop_ship_address(frm);
     }
 });
+
+function set_drop_ship_address(frm) {
+    if (frm.doc.drop_ship_reference) {
+        frappe.call({
+            'method': 'frappe.client.get',
+            'args': {
+                'doctype': "Delivery Note",
+                'name': frm.doc.drop_ship_reference
+            },
+            'callback': function(response) {
+                if (response.message) {
+                    cur_frm.set_value('drop_ship_address', response.message.shipping_address_name);
+                    cur_frm.set_value('drop_ship_address_display', response.message.shipping_address);
+                }
+            }
+        });
+    } else {
+        cur_frm.set_value("drop_ship_address", null);
+        cur_frm.set_value("drop_ship_address_display", null);
+        cur_frm.set_value("drop_ship_customer", null);
+    }
+}
