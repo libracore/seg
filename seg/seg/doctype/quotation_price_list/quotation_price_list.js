ecf66b4e9114cbdd042e4b075a1ec508de887858
Ivanios <ivan.lochbihler@libracore.com>
2025-03-25 15:54:25 +0100

    removing items

--- seg/seg/doctype/quotation_price_list/quotation_price_list.js
+++ seg/seg/doctype/quotation_price_list/quotation_price_list.js
@@ -91,6 +91,15 @@ frappe.ui.form.on('Quotation Price List', {
     }
 });
 
+frappe.ui.form.on('Quotation Price List Templates', {
+    before_templates_remove(frm, cdt, cdn) {
+        let template_row = frappe.get_doc(cdt, cdn);
+        //Remove Items releated to template
+        remove_items(frm, template_row.item_code);
+        frm.refresh_field('items');
+    }
+});
+
 frappe.ui.form.on('Quotation Price List Items', {
     item_price(frm, cdt, cdn) {
         let row = frappe.get_doc(cdt, cdn);
@@ -262,5 +271,18 @@ function set_customer_group(sales_person) {
         });
     } else {
         cur_frm.set_value("customer_group", null);
+    }
+}
+
+function remove_items(frm, template) {
+    console.log("Start");
+    console.log(frm.doc.items.length);
+    for (let i = frm.doc.items.length -1; i <= 0; i--) {
+        console.log(i);
+        if (frm.doc.items[i].variant_of == template) {
+            console.log(frm.doc.items[i].idx);
+            let items_row = frm.doc.items[i];
+            frm.doc.items.splice(i, 1);
+        }
     }
 }
