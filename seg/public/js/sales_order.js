06c0bac3d57ad29c53ddd38da14cc9ef2b69b70b
Ivanios <ivan.lochbihler@libracore.com>
2025-04-09 11:03:55 +0200

    added custom scripts and lsva to Quotation sales order and delivery note

--- seg/public/js/sales_order.js
+++ seg/public/js/sales_order.js
@@ -3,6 +3,62 @@ frappe.ui.form.on('Sales Order',  {
         if (frm.doc.customer) {
             check_cash_discount(frm);
         }
+        
+        //Set picked up if customer is marked as "always picks up"
+        if (cur_frm.doc.__islocal) {
+            if (frm.doc.customer) {
+                check_pick_up(frm.doc.customer);
+            } else {
+                cur_frm.set_value("picked_up" , 0)
+            }
+        }
+        
+        if ((frm.doc.sales_team) && (frm.doc.sales_team.length === 0) && (frm.doc.customer)) {
+            // fetch sales team from customer
+            frappe.call({
+                "method": "frappe.client.get",
+                "args": {
+                    "doctype": "Customer",
+                    "name": frm.doc.customer
+                },
+                "callback": function(response) {
+                    var customer = response.message;
+                    if (customer.sales_team) {
+                        for (var i = 0; i < customer.sales_team.length; i++) {
+                            var child = cur_frm.add_child('sales_team');
+                            frappe.model.set_value(child.doctype, child.name, 'sales_person', customer.sales_team[i].sales_person);
+                            frappe.model.set_value(child.doctype, child.name, 'allocated_percentage', customer.sales_team[i].allocated_percentage);
+                        }
+                        cur_frm.refresh_field('sales_team');
+                    } 
+                }
+            });
+        }
+    },
+    delivery_date: function(frm) {
+        frm.doc.desired_date = frm.doc.delivery_date;
+    },
+    customer: function(frm) {
+        //Set picked up if customer is marked as "always picks up"
+        if (frm.doc.customer) {
+            check_pick_up(frm.doc.customer);
+        } else {
+            cur_frm.set_value("picked_up" , 0)
+        }
+    },
+	before_save: function(frm) {
+        // update VOC
+        update_voc(frm);
+        if (frm.doc.picked_up == 1) {
+            frm.doc.taxes.forEach(function(entry) {
+               if (entry.account_head == "2209 Geschuldete LSVA - SEG") {
+                   frappe.model.set_value("Sales Taxes and Charges", entry.name, 'tax_amount', 0);
+               } 
+            });
+        } else {
+            // get total weight, update weight and LSVA
+            getTotalWeight();
+        }
     }
 });
 
