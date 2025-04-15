76c1d155cc5ecfaa30b4a713a70238da5445974a
Ivanios <ivan.lochbihler@libracore.com>
2025-04-15 15:33:07 +0200

    added styles

--- seg/seg/report/beschaffungsplan/beschaffungsplan.py
+++ seg/seg/report/beschaffungsplan/beschaffungsplan.py
@@ -19,15 +19,15 @@ def execute(filters=None):
 
 def get_columns():
     return [
-        {"label": _("Item Code"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 200},
+        {"label": _("Item Code"), "fieldname": "item_code", "fieldtype": "Link", "options": "Item", "width": 20},
         {"label": _("Item Code"), "fieldname": "item_code_data", "fieldtype": "Data", "width": 100},
         {"label": _("Item Name"), "fieldname": "item_name", "fieldtype": "Data",  "width": 180},
         {"label": _("Default Supplier"), "fieldname": "default_supplier", "fieldtype": "Link", "options": "Supplier",  "width": 120},
         {"label": _("Stock UOM"), "fieldname": "stock_uom", "fieldtype": "Link", "width": 80, "options": "UOM"},
-        {"label": _("Stock in SKU"), "fieldname": "stock_in_sku", "fieldtype": "Float", "width": 80},   
-        {"label": _("Ordered Qty"), "fieldname": "ordered_qty", "fieldtype": "Float", "width": 80},
+        {"label": _("Stock in SKU"), "fieldname": "stock_in_sku", "fieldtype": "Int", "width": 80},   
+        {"label": _("Ordered Qty"), "fieldname": "ordered_qty", "fieldtype": "Int", "width": 80},
         {"label": _("Lead Time in Days"), "fieldname": "lead_time_days", "fieldtype": "int", "width": 100},
-        {"label": _("Avg Consumption"), "fieldname": "avg_consumption_per_day", "fieldtype": "Float", "width": 100},
+        {"label": _("Avg Consumption per Day"), "fieldname": "avg_consumption_per_day", "fieldtype": "Float", "width": 100},
         {"label": _("Stock End Date"), "fieldname": "days_until_stock_ends", "fieldtype": "Data", "width": 100},
         {"label": _("To Order"), "fieldname": "to_order", "fieldtype": "Check", "width": 50}
     ]
