cd04d3b34da09878ce707b71eaa6fbdd241c3b80
Gokuflynn <ivan.lochbihler@libracore.com>
2026-06-18 17:25:37 +0200

    created pdf

--- seg/seg/report/sales_overview/sales_overview.py
+++ seg/seg/report/sales_overview/sales_overview.py
@@ -144,8 +144,9 @@ def get_child_groups(item_group, item_groups):
 
 @frappe.whitelist()
 def send_report():
-    data = "Hallo"
-    header_data = {'from_date': "01.01.2025", 'to_date': "31.01.2025"}
+    filters = {'from_date': '2026-01-01', 'to_date': '2026-06-18', 'employee': 'Christian Aeschlimann', 'depth': '3 - Product Subcategory'}
+    data = get_data(filters)
+    header_data = {'from_date': "01.01.2025", 'to_date': "31.01.2025", 'actual_year': 2026, 'previous_year': 2025}
     overview_html = frappe.render_template("seg/seg/report/sales_overview/sales_overview.html", {'data': data, 'header_data': header_data})
     pdf = get_pdf(overview_html, options={"orientation": "Landscape"})
     # ~ frappe.local.response.filename = "overview.pdf"
